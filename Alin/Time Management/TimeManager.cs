bcabc893cf70fa330dd32ee6e8db6d0358c017e6
Yamihiro <dicualinleon@gmail.com>
2021-12-20 19:37:59 +0200

    Design pattern + Diagram

---
+++ Alin/Time Management/TimeManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormApp.Utils
+{
+    public class TimeManager : Singleton<TimeManager>
+    {
+        private TimeManager()
+        {
+
+        }
+
+        public new static TimeManager Instance
+        {
+            get
+            {
+                TimeManager.setCtor(() =>
+                {
+                    return new TimeManager();
+                });
+                return Singleton<TimeManager>.Instance;
+            }
+        }
+    }
+}
