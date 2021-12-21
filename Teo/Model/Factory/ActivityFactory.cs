9e994c2beece0c8989470202032dc41b6888bfce
mihaialexandruteodor <mihaialexandruteodor@gmail.com>
2021-12-20 22:05:31 -0800

    minor modif

---
+++ Teo/Model/Factory/ActivityFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WinFormApp.Model.Implement;
+using WinFormApp.Model.Interface;
+
+namespace WinFormApp.Model.Factory
+{
+    internal class ActivityFactory : ActivityFactoryAbstract
+    {
+        public override IActivity CreateActivity()
+        {
+            return new ActivityImplement();
+        }
+    }
+}
---
+++ Teo/Model/Implement/TeamActivityImplement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WinFormApp.Model.Interface;
+
+namespace WinFormApp.Model.Implement
+{
+    public class TeamActivityImplement : ActivityImplement
+    {
+        public override string getActivityName()
+        {
+            return "Team Activity";
+        }
+    }
+}
