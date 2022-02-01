4ef274171cd074e7622ab169e68ce0c8a05a2fa7
Yamihiro <dicualinleon@gmail.com>
2022-02-01 12:27:04 +0200

    Base interfaces with functionalities for Time Management

---
+++ WinFormApp/WinFormApp/Time Management/Interfaces/IActivityCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormApp
+{
+    public interface IActivityCategory
+    {
+        String CategoryName { get; }
+        public bool IsEqual(IActivityCategory otherPreference)
+        {
+            return CategoryName.Equals(otherPreference.CategoryName);
+        }
+    }
+}
---
+++ WinFormApp/WinFormApp/Time Management/Interfaces/IApplication.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormApp.TimeManagement
+{
+    public interface IApplication
+    {
+        public List<IActivity> GetActivities(List<IEvent> events);
+        public List<IActivity> GetActivities(String startTime, String endTime);
+        public List<IActivityCategory> GetUserPreferences();
+    }
+}
